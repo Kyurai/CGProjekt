61c0862d6eee9dfa6465cc7346a2b7bf8f8a40a6
Kyurai <dustinhellmann@t-online.de>
2017-06-07 17:39:25 +0200

    Added myGLWidget Class and set up mainwindow.ui

---
+++ myglwidget.h
@@ -0,0 +1,14 @@
+#ifndef MYGLWIDGET_H
+#define MYGLWIDGET_H
+
+#include <QWidget>
+#include <QOpenGLWidget>
+
+class MyGLWidget : public QOpenGLWidget
+{
+public:
+    MyGLWidget();
+    MyGLWidget(QWidget *parent):QOpenGLWidget(parent){}
+};
+
+#endif // MYGLWIDGET_H
